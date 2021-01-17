49a8f4dcc0059fc2cde7a2f3b176687044115449
avemike <fajny.michal.gajek@gmail.com>
2021-01-18 00:56:15 +0100

    Fix rand() by adding seed

--- src/modules/noise.c
+++ src/modules/noise.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
+#include <time.h>
+
 #include "bmp.h"
+
 //
 // Created by avemike on 16.01.2021.
 //
@@ -8,9 +11,14 @@
 Pixel *createNoise(int width, int height) {
     Pixel *data = (Pixel *) malloc(sizeof(Pixel) * width * height);
 
+    // random generator helpers
+    int seed;
+    time_t tt;
+    seed = time(&tt);
+    srand(seed);
     for (int row = 0; row < height; row++) {
         for (int column = 0; column < width; column++) {
-            int random = rand() % 256;
+            unsigned char random = rand() % 256;
             data[column + row * width].blue = random;
             data[column + row * width].green = random;
             data[column + row * width].red = random;
